c688b858b99bcb46abd35f3af4027e3b4f700f71
Yateen Kedare <yateenkedare95@gmail.com>
2024-02-06 07:11:21 +0000

    temp commit for valentines

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,27 @@
-
-import ImageSelector from '../ImageSelector/ImageSelector';
+import { useState } from 'react';
+import Ask from '../Ask/Ask';
+import Password from '../Password/Password';
+import Success from '../Success/Success';
 import './App.css';
 
 function App() {
+  const [stage, setStage] = useState(0);
+
+  const updateStage = () => {
+    setStage(stage + 1);
+  }
+
+  const resetStage = () => {
+    setStage(0);
+  }
+
   return (
-    <div className="App">
-      <ImageSelector
-        imageUrl1="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
-        altText1="Image 1"
-        imageUrl2="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
-        altText2="Image 2" />
+    <div className="centered-container">
+      <div className="valentine-container">
+        {stage === 0 && <Password goToNextStage={updateStage}/>}
+        {stage === 1 && <Ask goToNextStage={updateStage}/>}
+        {stage === 2 && <Success goToFirstStage={resetStage}/>}
+      </div>
     </div>
   );
 }
